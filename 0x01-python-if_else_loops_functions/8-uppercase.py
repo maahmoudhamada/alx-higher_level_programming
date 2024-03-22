f69454fd1a0b92fa32965c0e571af4f12035c277
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-03-22 05:12:55 +0200

    0x01 -> 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+
+    for x in str:
+        if ord(x) >= ord('a') and ord(x) <= ord('z'):
+            print("{:c}".format(ord(x) - 32), end="")
+        else:
+            print("{}".format(x), end="")
+    print()
