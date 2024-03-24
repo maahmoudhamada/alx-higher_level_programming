3bf36fdfafefc458d9b76224e2da5228eb31f29b
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-03-25 00:17:24 +0200

    0x03 -> 4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+
+    lst_cpy = my_list.copy()
+    if idx < 0 or idx >= len(my_list):
+        return lst_cpy
+    else:
+        for x in lst_cpy:
+            if x == idx:
+                lst_cpy[x] = element
+        return lst_cpy
