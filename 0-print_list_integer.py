a1cfb00e0f3298a442534ce0ba2bea181ad3e4a5
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-03-25 09:42:43 +0200

    0x03 -> Task "6, 7"

--- 0-print_list_integer.py
+++
@@ -1,8 +0,0 @@
-Enter git message
-[main 0d59687] 0x03 -> 0-print_list_integer.py
- 5 files changed, 13 insertions(+)
- create mode 100644 0-print_list_integer.py
- create mode 100755 0x03-python-data_structures/0-main.py
- create mode 100755 0x03-python-data_structures/0-print_list_integer.py
- create mode 100644 0x03-python-data_structures/README.md
- create mode 100644 0x03-python-data_structures/__pycache__/0-print_list_integer.cpython-310.pyc
