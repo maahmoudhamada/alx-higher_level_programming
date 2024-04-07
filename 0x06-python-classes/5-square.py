6d7166d513e9b2ea4352d3725504bb74ab137964
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-04-08 01:25:22 +0200

    0x06 -> 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+"""Module containg class 'Square'"""
+
+
+class Square:
+    """Square: a Class"""
+
+    def __init__(self, size=0):
+        """
+        __init__: Constructor for 'Square' class
+        Args:
+            size (int): Size of square
+        Return:
+            None
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """area: Public instance method
+        Return: Area of a square (int)
+        """
+        return pow(self.__size, 2)
+
+    @property
+    def size(self):
+        """size: Getter method
+        Return: current instance attribute 'size'
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size: Setter method
+        Args:
+            value: Value of square's size to be setted
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """my_print: Method to print a square with char: '#'"""
+        if self.__size == 0:
+            print()
+        for h in range(self.__size):
+            for w in range(self.__size):
+                print("{}".format('#'), end="")
+            print()
