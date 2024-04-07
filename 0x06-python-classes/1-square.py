bf9f02355851cd7903e410691048741aece1d1e9
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-04-08 00:05:16 +0200

    0x06 -> 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+"""Module containg class 'Square'"""
+
+
+class Square:
+    """Square: a Class"""
+
+    def __init__(self, sz):
+        """
+        __init__: Constructor for 'Square' class
+
+        Args:
+            sz (int): Size of square
+
+        Return:
+            None
+        """
+        self.__size = sz
