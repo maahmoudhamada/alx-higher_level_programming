9c8528d54795930cd32f2ae0ab4afd092c4ab617
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-04-09 15:39:37 +0200

    Update: 0x08 -> 9-rectangle.py "Switching width with height"

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -15,8 +15,8 @@ class Rectangle:
             height: Height of a rectangle
         """
 
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     def __str__(self):
