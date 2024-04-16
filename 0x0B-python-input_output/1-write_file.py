a1ef14a981e24e06cc4b8298bd0764764a52e1a9
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-04-16 13:10:28 +0200

    0x0B -> 1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""The write_file module"""
+
+
+def write_file(filename="", text=""):
+    """Write into an object's stream (file)
+    Args:
+        filename: Name of file to open
+        text: Content to be writtern into a file
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        charNum = f.write(text)
+    return charNum
