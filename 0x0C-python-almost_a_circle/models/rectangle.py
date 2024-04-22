c545a907171d2e46c7b21dc08695c772ca5217ec
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-04-22 19:13:24 +0200

    0x0C -> rectangle.py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+from models.base import Base
+"""The Rectangle Class Module"""
+
+
+class Rectangle(Base):
+    """Rectangle Class"""
+
+    # ========================  Init  ========================
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Constructor"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    # ========================  Width  ========================
+
+    @property
+    def width(self):
+        """Width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Width setter"""
+        self.__width = value
+
+    # ========================  Height  ========================
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        self.__height = value
+
+    # ========================  X  ========================
+
+    @property
+    def x(self):
+        """x getter"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """x setter"""
+        self.__x = value
+
+    # ========================  Y  ========================
+
+    @property
+    def y(self):
+        """y getter"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """y setter"""
+        self.__y = value
