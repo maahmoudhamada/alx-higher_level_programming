acd951679e6476c6ec7ea77194880194ae83d2c8
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-04-22 22:45:09 +0200

    Adding: 0x0C -> base.py "save_to_file"

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,6 +8,8 @@ class Base:
 
     __nb_objects = 0
 
+    # ========================  Init  ========================
+
     def __init__(self, id=None):
         """Constructor method to build base class
         Args:
@@ -19,6 +21,8 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    # ========================  to_json_string  ========================
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """Return JSON representation"""
@@ -26,3 +30,13 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    # ========================  save_to_file  ========================
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save JSON strings into class files"""
+        if list_objs is not None:
+            final_lst = [x.to_dictionary() for x in list_objs]
+            with open("{}.json".format(cls.__name__), 'w+', encoding="utf-8") as f:
+                f.write(cls.to_json_string(final_lst))
