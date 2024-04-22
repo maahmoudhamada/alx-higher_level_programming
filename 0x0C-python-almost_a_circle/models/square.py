cdd7a65b9e18446e14905b69f04a469a4832ca33
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-04-22 20:27:33 +0200

    Adding: 0x0C -> square.py "getter/setter"

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,3 +14,14 @@ class Square(Rectangle):
         """string method to print attributes"""
         return "[Square] ({}) {}/{} - {}" \
             .format(self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """Size getter"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Size setter"""
+        self.width = value
+        self.height = value
