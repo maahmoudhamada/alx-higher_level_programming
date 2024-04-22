f29f6a1026f6b76508a38c3b83198a4a2fc334dd
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-04-22 20:01:14 +0200

    0x0C -> square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+"""The Square Class Module"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square subclass from Rectangle superclass"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Constructor"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """string method to print attributes"""
+        return "[Square] ({}) {}/{} - {}" \
+            .format(self.id, self.x, self.y, self.width)
