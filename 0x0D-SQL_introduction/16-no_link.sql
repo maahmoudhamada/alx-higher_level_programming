c02bfb8c7bc7a0229a1a1612ce1f0f1449fe4fc7
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-06-14 07:26:34 +0300

    0x0D -> 16-no_link.sql "Editing"

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,9 +1,2 @@
 -- Script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
-DELETE FROM `second_table`
-WHERE name = "GEORGE";
-
-INSERT INTO `second_table` VALUES(3, "Aria", 18);
-INSERT INTO `second_table` VALUES(4, "Aria", 12);
-
-SELECT score, name FROM `second_table`
-ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name!='' OR name IS NOT NULL ORDER BY score DESC;
