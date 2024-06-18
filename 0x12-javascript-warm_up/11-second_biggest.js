22e49c43829106aa5f8dac1abf72427b566fd42a
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-06-18 18:04:57 +0300

    0x12 -> 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+const array = process.argv.slice(2);
+const len = array.length;
+let tmp;
+
+if (len === 0 || len === 1) {
+  console.log(0);
+} else {
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      if (array[j] > array[j + 1]) {
+        tmp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = tmp;
+      }
+    }
+  }
+  console.log(array[len - 2]);
+}
