4ae6c630cb50bc962fbed7b45162dfa07781eff2
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-06-22 14:16:07 +0300

    0x0F -> 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""script takes in an arg and displays values where name matches the args."""
+
+import MySQLdb
+import sys
+import re
+
+if __name__ == '__main__':
+    av = sys.argv
+    sql_user, sql_pass, db_name, name_search = av[1], av[2], av[3], av[4]
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sql_user,
+        passwd=sql_pass,
+        db=db_name)
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states
+        WHERE name='{}'""".format(name_search))
+    print(cur.fetchone())
+    cur.close()
+    db.clode()
