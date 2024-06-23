18f498668242fea713bfa9a400a8e70fcdabeca5
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-06-23 15:28:54 +0300

    0x0F -> model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""The State class module"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+import sqlalchemy
+import sys
+
+data = 'mysql+mysqldb://{}:{}@localhost:3306/{}'\
+    .format(sys.argv[1], sys.argv[2], sys.argv[3])
+engine = sqlalchemy.create_engine(data)
+engine.connect()
+Base = declarative_base()
+
+
+class State(Base):
+    """State subclass inherting from Base class"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+
+Base.metadata.create_all(engine)
