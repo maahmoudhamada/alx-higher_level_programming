08b6ba6d56e6c0bc83fa6057e6da27b6babec259
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-06-26 02:16:57 +0300

    0x0F -> 7-model_state_fetch_all.py

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Script to list all rows in a table `State`"""
+
+
+import sys
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    connect_info = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.\
+        format(sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = sqlalchemy.create_engine(connect_info, pool_pre_ping=True)
+    engine.connect()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for name, id in \
+            session.query(State.name, State.id).order_by(State.id).all():
+        print("{}: {}".format(id, name))
