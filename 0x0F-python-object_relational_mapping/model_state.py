2d0e163c323532360c3c4d08d1f7a6a3d1cd6a87
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-06-25 23:30:08 +0300

    0x0F -> model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""The State class module"""
+"""The model_state module"""
 
-from sqlalchemy import Column, Integer, String
+
+import sys
+import sqlalchemy
+from sqlalchemy import Integer, String, Column
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
-    """State subclass inherting from Base class"""
+    """State class represent db table"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
